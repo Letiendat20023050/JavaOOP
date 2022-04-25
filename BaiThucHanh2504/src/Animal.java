0021dd6070178ce5c94f1093c1ceea783b433fdd
Letiendat20023050 <letiendat20023050@gmail.com>
2022-04-25 20:22:40 +0700

    Baithuchanh2504

---
+++ BaiThucHanh2504/src/Animal.java
@@ -0,0 +1,7 @@
+abstract class Animal {
+    abstract void Eat();
+    abstract void sleep();
+    abstract void walk();
+    abstract void run();
+    abstract void roar();
+}
---
+++ BaiThucHanh2504/src/Tiger.java
@@ -0,0 +1,23 @@
+public class Tiger extends Animal{
+    public static void main(String[] args){
+    }
+    void Eat() {
+        System.out.println(" an thit");
+    }
+
+    void sleep() {
+        System.out.println(" dang ngu");
+    }
+
+    void walk() {
+        System.out.println(" walks");
+    }
+
+    void run() {
+        System.out.println(" dang chay");
+    } 
+
+    void roar() {
+         System.out.println(" ....");
+     }
+}
